ba396037380ea4fccfb6036974d135b6fdfd4893
AhnJoonSung <tjdtna01@naver.com>
2024-01-09 00:32:12 +0900

    spend_time refactoring

--- philo/timer.c
+++ philo/timer.c
@@ -6,14 +6,16 @@
 /*   By: ahn <ahn@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 15:32:10 by jooahn            #+#    #+#             */
-/*   Updated: 2024/01/08 21:41:02 by ahn              ###   ########.fr       */
+/*   Updated: 2024/01/09 00:27:01 by ahn              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+static int	get_acttime(t_data *data, int status);
+
 // return milli seconds
-long	get_time(void)
+long get_time(void)
 {
 	static long			start_time;
 	static t_timeval	start;
@@ -33,22 +35,28 @@ long	get_time(void)
 void	spend_time(t_data *data, long start, int status)
 {
 	long	until;
-	long	gap_time;
 
-	until = 0;
-	if (status == EATING)
-		until = (start + data->time_to_eat);
-	else if (status == SLEEPING)
-		until = (start + data->time_to_sleep);
-	else if (status == WAIT)
-	{
-		if (data->time_to_eat < data->time_to_sleep)
-			return ;
-		gap_time = data->time_to_eat - data->time_to_sleep;
-		until = start + gap_time + 1;
-	}
-	else if (status == MONITORING)
-		until = start + 9;
+	until = start + get_acttime(data, status);
 	while (get_time() < until)
 		usleep(FT_ATOMIC_TIME);
+}
+
+static int	get_acttime(t_data *data, int status)
+{
+	long	gap_time;
+
+	if (status == EATING)
+		return (data->time_to_eat);
+	if (status == SLEEPING)
+		return (data->time_to_sleep);
+	if (status == WAIT)
+	{
+		if (data->time_to_eat < data->time_to_sleep)
+			return (0);
+		gap_time = data->time_to_eat - data->time_to_sleep;
+		return (gap_time + 1);
+	}
+	if (status == MONITORING)
+		return (9);
+	return (0);
 }
